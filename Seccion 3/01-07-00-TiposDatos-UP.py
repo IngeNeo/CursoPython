4bb39889105425c7716b8a4873ee2f1f9cf9d8dc
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-28 15:53:43 -0500

    Seccion3

---
+++ Seccion 3/01-07-00-TiposDatos-UP.py
@@ -0,0 +1,16 @@
+#Tipo String
+x: str = "Inge"  # x. str es una pista visual para ver que tipo de dato se va a usar, peor no influye en el 				   tipo de dato que pueda almacenar
+print(x)
+print(type(x))
+#Tipo Entero
+x = 2
+print(x)
+print(type(x))
+#Tipo Float
+x = 3.5
+print(x)
+print(type(x))
+#Tipo booleano
+x = False
+print(x)
+print(type(x))
---
+++ Seccion 3/01-09-00-TiposString-UP.py
@@ -0,0 +1,4 @@
+#Cadena (String)
+miGrupoFavorito = "Jesus Legacy"
+comentario = "The Best Gospel Band"
+print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
