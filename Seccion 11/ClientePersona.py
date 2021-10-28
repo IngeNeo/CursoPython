783daa0feffadab9948f2a20634c2bde8e21a890
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-10-28 16:12:48 -0500

    Seccion 11

---
+++ Seccion 11/ClientePersona.py
@@ -0,0 +1,7 @@
+from Persona import *
+
+persona1 = Persona('Roger', 28)
+print(persona1)
+
+empleado1 = Empleado('Isabel', 30, 5000)
+print(empleado1)
---
+++ Seccion 11/PruebaPersona.py
@@ -0,0 +1,8 @@
+from Persona import Persona
+
+print('Creación objetos'.center(30,'-'))
+persona1 = Persona('Isabel', 'Miranda', 30)
+persona1.mostrar_detalle()
+
+print('Eliminación objetos'.center(30,'-'))
+del persona1
